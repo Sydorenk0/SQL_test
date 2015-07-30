11b9a2a76112dd8728b7c689caa422247509a913
Sydorenk0 <alexandr.sydorenko@gmail.com>
2015-07-30 17:15:10 +0300

    SQL test
    Прога  с SQL запросами.

---
+++ SQL_lesson1/src/test/resources/test.sql
@@ -0,0 +1,5 @@
+#insert into userlist (login, lastName, Email) values('gin', 'last2', 'Gin@');
+#select * from userlist;
+#select * from userlist where login = 'login2';
+update userlist set login = 'jan', lastName = 'last1', Email = 'jan@' where id=2;
+#delete from userlist where id=3;
